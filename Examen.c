50e924c9fcae638ca75a52a111dabf1d5f0c01c0
Laboratorio <laboratorio@udla-ec.int>
2023-07-10 15:56:51 -0500

    Uso de estructura para guardar los nombres y notas

---
+++ Examen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define Nombre 50
+#define Materia 50
+
+typedef struct
+{
+    char nombre[Nombre];
+    char materia[Materia];
+    float nota1;
+    float nota2;
+    float nota3;
+} Estudiante;
+
